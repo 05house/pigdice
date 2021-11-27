857aeca00ee38d868cd2062ad4e14524eb965d67
juhan00 <jvabya@gmail.com>
2021-11-27 22:16:50 +0900

    feat: Add game player function
    set player number add

--- pigdice.js
+++ pigdice.js
@@ -1,11 +1,13 @@
 let score = 0;
 let randNum = 0;
+let totalPlayer = 5;
+let player = 1;
 
 function rand(){
 	return Math.trunc(Math.random()*6)+1;
 }
 
-function reset(){
+function scoreReset(){
 	score = 0;
 	randNum = 0;
 }
@@ -15,15 +17,23 @@ function pigdiceStart(){
 		randNum = rand();
 
 		if(randNum === 1){
-			console.log("숫자1, 다음 플레이어 턴");
-			reset();
+			console.log(player + "번 플레이어 숫자1, 다음 플레이어 턴");
+			scoreReset();
+			
+			if(player < totalPlayer){
+				player++;
+			}else{
+				player = 1;
+			}
+
 		}else if(score >= 100){
-			console.log("플레이어 승리!!!");
-			reset();
+			console.log(player + "번 플레이어 승리!!!");
+			scoreReset();
+			player = 1;
 			break;
 		}else{
 			score += randNum;
-			console.log(score);
+			console.log(player + "번 플레이어 숫자" + randNum + " 합계:" + score);
 		}
 	}
 }
