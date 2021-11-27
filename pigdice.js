0706231a3cffe2c595d77aa1fb50337a138948ff
juhan00 <jvabya@gmail.com>
2021-11-27 21:45:30 +0900

    feat: Add game logic
    Add case random score, number1, win

--- pigdice.js
+++ pigdice.js
@@ -1,5 +1,31 @@
+let score = 0;
+let randNum = 0;
+
 function rand(){
 	return Math.trunc(Math.random()*6)+1;
 }
 
-console.log(rand());
+function reset(){
+	score = 0;
+	randNum = 0;
+}
+
+function pigdiceStart(){
+	while(randNum !== 1){
+		randNum = rand();
+
+		if(randNum === 1){
+			console.log("숫자1, 다음 플레이어 턴");
+			reset();
+		}else if(score >= 100){
+			console.log("플레이어 승리!!!");
+			reset();
+			break;
+		}else{
+			score += randNum;
+			console.log(score);
+		}
+	}
+}
+
+pigdiceStart();
