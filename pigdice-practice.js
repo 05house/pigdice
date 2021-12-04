7267e573c4c68834ec7f49b03acd2cdde65bec3a
05Castle <kos4005@gmail.com>
2021-12-04 21:37:01 +0900

    feat: Add score system
    100점까지 점수 더해가는 기능 추가

--- pigdice-practice.js
+++ pigdice-practice.js
@@ -2,4 +2,15 @@ function rollDice(){
 	return Math.floor(Math.random()*6)+1;
 }
 
-console.log(rollDice())
+function gameStart() {
+	for (score=0; score < 100; score += diceNum) {
+	diceNum = rollDice()
+	console.log("점수" + score)
+	console.log("주사위값" + diceNum)
+	}
+	if (score >= 100) {
+		console.log(score +"달성. 승리했습니다!")
+	}
+}
+
+gameStart()
