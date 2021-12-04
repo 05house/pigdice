22e1ae50403ef982dd3839e0a681005010b4a6a2
juhan00 <jvabya@gmail.com>
2021-12-05 03:11:56 +0900

    feat: Add function hold and new game
    Change player score to array

--- pigdice-practice.js
+++ pigdice-practice.js
@@ -1,70 +1,82 @@
-let score = 0;
-let randNum = 0;
-let totalPlayer = 2;
-let player = 1;
-
 function rand(){
 	return Math.floor(Math.random()*6)+1;
 }
 
-const allPlayerCountEl = document.querySelectorAll('.player_group li .count');
 
-function scoreReset(){
-	score = 0;
-	randNum = 0;
+let score = [];
+let randNum = 0;
+let totalPlayer = 2;
+let currentPlayer = 1;
 
-	allPlayerCountEl.forEach(function (playerCount) {
-    playerCount.innerText = 0;
-  });
+for(let i=0; i < totalPlayer; i++){
+	score.push(0);
+}
 
+const allButtonEl = document.querySelectorAll('button');
+
+function buttonLock(target){
+	target.disabled = true;
+}
+
+function buttonUnlock(){
+	allButtonEl.forEach(function(allButtonEl){
+		allButtonEl.disabled = false;
+	});	
 }
 
 function newGame(){
-	score = 0;
-	randNum = 0;
-	player = 1;
-
-	allPlayerCountEl.forEach(function (playerCount) {
-    playerCount.innerText = 0;
+	const allPlayerCountEl = document.querySelectorAll('.player_group li .count');
+	allPlayerCountEl.forEach(function (allPlayerCountEl) {
+    allPlayerCountEl.innerText = 0;
   });
+	const diceEl = document.querySelector('.dice span');
+	diceEl.innerText = 0;
 }
 
 
-function playTurnChange(){	
-	const playerGroupEl = document.querySelector('.player_group');
-
-	if(player < totalPlayer){
-		player++;
-	}else{
-		player = 1;
+function playTurnChange(hold){	
+	const currentPlayerCountEl = document.querySelector('.player_group li.active .count');
+	if(!hold){
+		score[currentPlayer - 1] = 0;
+		currentPlayerCountEl.innerText = 0;
 	}
 
-	const currentPlayerEl = document.querySelector('.player_group li:nth-child(' + player + ')');
+	if(currentPlayer < totalPlayer){
+		currentPlayer++;
+	}else{
+		currentPlayer = 1;
+	}
+
+	const nextPlayerEl = document.querySelector('.player_group li:nth-child(' + currentPlayer + ')');
 	const allPlayerEl = document.querySelectorAll('.player_group li');
 
 	allPlayerEl.forEach(function (playerEl) {
     playerEl.classList.remove('active');
   });
 
-	currentPlayerEl.classList.add('active');
+	nextPlayerEl.classList.add('active');
 
-	scoreReset();
+	buttonUnlock();
+	
 }
 
 function scoreChange(randNum){
-	score += randNum;
-	const currentPlayerCountEl = document.querySelector('.player_group li.active .count');
-	// console.log(player + "번 플레이어 숫자" + randNum + " 합계:" + score);
-	currentPlayerCountEl.innerText = score;
+	playerScore = score[currentPlayer - 1];
+	score[currentPlayer - 1] = playerScore + randNum;
 
-	if(score >= 100){
-		//console.log('a');
+	const currentPlayerCountEl = document.querySelector('.player_group li.active .count');
+	currentPlayerCountEl.innerText = score[currentPlayer - 1];
+
+	if(score[currentPlayer - 1] >= 100){
+		alert(currentPlayer + '번 플레이어 승리!!!');
+
 		delay(function(){
-			alert(player + '번 플레이어 승리!!!');
+			newGame();
 		},300);
 		
-		//newGame();
 	}
+
+	buttonUnlock();
 }
 
 function delay(callback, time){
@@ -74,18 +86,17 @@ function delay(callback, time){
 }
 
 function pigdiceStart(){
+	newGame();
 
 	const btnRollDiceEl = document.querySelector('.roll_dice'); 
-
-	btnRollDiceEl.addEventListener('click', function(){
-		//if(randNum !== 1){
+	btnRollDiceEl.addEventListener('click', function(event){
+			buttonLock(event.target);
 			randNum = rand();
 			
 			const diceEl = document.querySelector('.dice span');
 			diceEl.innerText = randNum;
 
 			if(randNum === 1){
-				//console.log(player + "번 플레이어 숫자1, 다음 플레이어 턴");
 				delay(function(){
 					alert('숫자1 다음 플레이어 턴');
 					playTurnChange();
@@ -93,15 +104,21 @@ function pigdiceStart(){
 			}else{
 				delay(function(){
 					scoreChange(randNum);
-					// if(score >= 100){
-					// 	console.log('a');
-					// 	// alert(player + '번 플레이어 승리!!!');
-					// 	newGame();
-					// }
 				},200);
 			}
-		//}
 
+	});
+
+	const btnHoldEl = document.querySelector('.hold'); 
+	btnHoldEl.addEventListener('click', function(){
+		delay(function(){
+			playTurnChange('hold');
+		},300);
+	});
+
+	const btnNewGameEl = document.querySelector('.new_game'); 
+	btnNewGameEl.addEventListener('click', function(){
+		newGame();
 	});
 	
 }
