83a84fd433470504f56e35fbdbc012b5f3fdc3df
05Castle <kos4005@gmail.com>
2021-12-04 23:21:53 +0900

    feat: Print score on the screen
    점수가 화면에 출력되로록 기능 추가

--- pigdice-practice.js
+++ pigdice-practice.js
@@ -1,4 +1,5 @@
 const button = document.querySelector('button')
+const scoreText = document.querySelector('.score')
 
 function rollDice (){
 	return Math.floor(Math.random()*6)+1;
@@ -7,21 +8,20 @@ function rollDice (){
 let score = 0
 
 function gameStart () {
-	while (score < 100) {
-		diceNum = rollDice()
-		score += diceNum
-		if (diceNum === 1) {
-			score = 0;
-			console.log("주사위값 : " + diceNum)
-			console.log("1이 나와서 주사위가 초기화 되었습니다")
-			break
-		} else {
-				console.log("주사위값 : " + diceNum)
-				console.log("점수 : " + score)
-		}
-	}
-	if (score >= 100) {
+	diceNum = rollDice ()
+	button.innerText = "주사위 굴리기"
+	if (diceNum === 1) {
+		score = 0;
+		console.log("주사위값 : " + diceNum)
+		scoreText.innerText = "1이 나와서 주사위가 초기화 되었습니다"
+	} else if (score >= 100) {
 		console.log("달성. 승리했습니다!")
+		scoreText.innerText = "100점 달성! 승리했습니다!!"
+	} else {
+		score += diceNum
+		console.log("주사위값 : " + diceNum)
+		console.log("점수 : " + score)
+		scoreText.innerText = `주사위값 : ${diceNum} / 총점 : ${score}` 
 	}
 }
 
