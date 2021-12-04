ad78f30d1919f5148fa8546dae92794915ae6a36
05Castle <kos4005@gmail.com>
2021-12-04 23:29:50 +0900

    refactor: delete console.log
    console.log 부분 전부 삭제

--- pigdice-practice.js
+++ pigdice-practice.js
@@ -12,18 +12,14 @@ function gameStart () {
 	button.innerText = "주사위 굴리기"
 	if (diceNum === 1) {
 		score = 0;
-		console.log("주사위값 : " + diceNum)
 		scoreText.innerText = "1이 나와서 주사위가 초기화 되었습니다"
 		button.innerText = " 다시 시작하기"
 	} else if (score >= 100) {
-		console.log("달성. 승리했습니다!")
 		alert('100점 달성! 승리했습니다!!')
 		button.innerText = " 다시 시작하기"
 		score = 0
 	} else {
 		score += diceNum
-		console.log("주사위값 : " + diceNum)
-		console.log("점수 : " + score)
 		scoreText.innerText = `주사위값 : ${diceNum} / 총점 : ${score}` 
 	}
 }
