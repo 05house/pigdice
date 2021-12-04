5af205e81449e7c97f5de65079df3fe86be84607
05Castle <kos4005@gmail.com>
2021-12-04 22:02:40 +0900

    feat: Add reset system
    주사위 값이 1이 나오면 점수가 0이 되고 멈추도록 기능을 추가함.

--- pigdice-practice.js
+++ pigdice-practice.js
@@ -2,14 +2,24 @@ function rollDice(){
 	return Math.floor(Math.random()*6)+1;
 }
 
+let score = 0
+
 function gameStart() {
-	for (score=0; score < 100; score += diceNum) {
-	diceNum = rollDice()
-	console.log("점수" + score)
-	console.log("주사위값" + diceNum)
+	while (score < 100) {
+		diceNum = rollDice()
+		score += diceNum
+		if (diceNum === 1) {
+			score = 0;
+			console.log("주사위값 : " + diceNum)
+			console.log("1이 나와서 주사위가 초기화 되었습니다")
+			break
+		} else {
+				console.log("주사위값 : " + diceNum)
+				console.log("점수 : " + score)
+		}
 	}
 	if (score >= 100) {
-		console.log(score +"달성. 승리했습니다!")
+		console.log("달성. 승리했습니다!")
 	}
 }
 
